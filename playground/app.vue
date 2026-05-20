ef3044f3ec1fbc69b5828c3f1ec923477f2499dc
Luis Ramirez Calle <luis.ramirezcalle@outlook.com>
2026-05-20 13:24:05 +0100

    Revert "feat: validate kernel integration with playground and e2e tests"
    This reverts commit 76baa9f56d58f06f6e8ab6b7fadbaadb9702d4bc.

--- playground/app.vue
+++ playground/app.vue
@@ -1,16 +1,8 @@
-<script setup lang="ts">
-import { counterToken } from './shared/providers/CounterProvider'
-
-const container = useContainer()
-const counter = container.make(counterToken)
-
-const firstValue = counter.next()
-const secondValue = counter.next()
-</script>
-
 <template>
   <div>
-    <span data-testid="first-value">{{ firstValue }}</span>
-    <span data-testid="second-value">{{ secondValue }}</span>
+    Nuxt module playground!
   </div>
 </template>
+
+<script setup>
+</script>
